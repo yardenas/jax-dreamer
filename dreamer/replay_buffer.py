1f2c642520067b5345ca269fef0b2f124cb331e7
yardenas <yardas@student.ethz.ch>
2021-11-30 18:05:08 +0100

    Update replay buffer after a short test session

--- dreamer/replay_buffer.py
+++ dreamer/replay_buffer.py
@@ -21,7 +21,7 @@ class ReplayBuffer(object):
                 jnp.nan, jnp.float32),
             'reward': jnp.full((capacity, max_episode_length) + (1,),
                                jnp.nan, jnp.float32),
-            'terminal': jnp.full((capacity, max_episode_length), (1,),
+            'terminal': jnp.full((capacity, max_episode_length) + (1,),
                                  jnp.nan, jnp.bool_)
         }
         self._episdoe_lengths = jnp.full((capacity,), 0, dtype=jnp.int64)
@@ -32,13 +32,15 @@ class ReplayBuffer(object):
 
     def store(self, transition: Transition):
         position = self._episdoe_lengths[self.idx]
-        for key, val in self.data.items():
-            val[self.idx, position] = transition[key]
-            self._episdoe_lengths[self.idx] += 1
+        for key in self.data.keys():
+            self.data[key] = self.data[key].at[self.idx, position].set(
+                transition[key])
+        self._episdoe_lengths = self._episdoe_lengths.at[self.idx].add(1)
         if transition['terminal'] or transition['info'].get(
                 'TimeLimit.truncated', False):
-            self.data['observation'][self.idx, position] = \
-                transition['next_observation']
+            self.data['observation'] = \
+                self.data['observation'].at[self.idx, position].set(
+                    transition['next_observation'])
             self.idx = int((self.idx + 1) % self.capacity)
 
     def sample(self, samples: int, length: int):
@@ -56,6 +58,15 @@ class ReplayBuffer(object):
         # episodes.
         # 2. Collect sequences from episodes.
         # 3. Repeat 1-2 until `batch size` of sequences is gathered.
+
+        def sample_episode_ids(key: jnp.ndarray,
+                               episode_lengths: jnp.ndarray, length: int):
+            out = jnp.where(episode_lengths >= length, 1, 0).astype(jnp.int64)
+            num_episodes = out.sum()
+            logits = jnp.log(episode_lengths[:num_episodes])
+            sample = jax.random.categorical(key, logits)
+            return (out * sample).astype(jnp.int64)
+
         def sample_sequence(key: jnp.ndarray,
                             episode_data: Mapping[str, jnp.ndarray],
                             episode_length: jnp.ndarray,
@@ -74,9 +85,9 @@ class ReplayBuffer(object):
             key, ids_key = jax.random.split(key)
             # Sample uniformly across observations within all episodes which are
             # long enough.
-            idxs: jnp.ndarray = self._sample_episode_ids(ids_key,
-                                                         episode_lengths,
-                                                         length)
+            idxs: jnp.ndarray = sample_episode_ids(ids_key,
+                                                   episode_lengths,
+                                                   length)
             sampled_episods = jax.tree_map(lambda x: x[idxs], data)
             episode_count += idxs.sum()
             key, *sequence_keys = jax.random.split(key, idxs.sum())
@@ -88,14 +99,6 @@ class ReplayBuffer(object):
         observation, action, reward, terminal = map(jnp.stack, zip(*episodes))
         return dict(observation=observation, action=action,
                     reward=reward, terminal=terminal)
-
-    def _sample_episode_ids(self, key: jnp.ndarray,
-                            episode_lengths: jnp.ndarray, length: int):
-        out = jnp.where(episode_lengths >= length, 1, 0).astype(jnp.int64)
-        num_episodes = out.sum()
-        logits = jnp.log(episode_lengths[:num_episodes])
-        sample = jax.random.categorical(key, logits)
-        return (out * sample).astype(jnp.int64)
 
     def __len__(self):
         return self._episdoe_lengths.sum()
