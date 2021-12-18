0d0b4b5a84e62687d43e2e40e4b58fbe8b33384c
yardenas <yardas@student.ethz.ch>
2021-12-18 22:24:47 +0100

    Bug fix

--- dreamer/models.py
+++ dreamer/models.py
@@ -82,10 +82,7 @@ class Actor(hk.Module):
         mu, stddev = jnp.split(mlp(observation), 2, -1)
         init_std = np.log(np.exp(5.0) - 1.0).astype(stddev.dtype)
         stddev = jnn.softplus(stddev + init_std) + self._min_stddev
-        multivariate_normal_diag = tfd.Normal(
-            loc=5.0 * jnn.tanh(mu / 5.0),
-            scale_diag=stddev
-        )
+        multivariate_normal_diag = tfd.Normal(5.0 * jnn.tanh(mu / 5.0), stddev)
         # Squash actions to [-1, 1]
         squashed = tfd.TransformedDistribution(multivariate_normal_diag,
                                                b.StableTanhBijector())
